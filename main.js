a35fd5174a5898809a222912cf8ce8e4da19adc1
cheng <chengharvp@gmail.com>
2024-07-29 17:33:16 -0400

    second commit

--- main.js
+++ main.js
@@ -1,31 +1,28 @@
-// This JS file will listen for a click on the side menu's links to then load the appropriate html file into the content box. 
-document.addEventListener("DOMContentLoaded", () => {
-    // These are variables that will keep track of the side-menu links and the content area
-    const links = document.querySelectorAll('.nav-link');
-    const contentArea = document.getElementById('content-area');
+const navLinks = document.querySelectorAll('.nav-link');
+const headerNavLink = document.querySelector('.header-nav-link');
+const footerNavLink = document.querySelector('.footer-nav-link');
 
-    // Load home.html initially when index.html is loaded
-    loadContent('content/home.html');
+window.onload = redirectIframe;
 
-    // This is a click listener
-    links.forEach(link => {
-        link.addEventListener('click', (event) => {
-            event.preventDefault();
-            const page = link.getAttribute('data-page');
-            loadContent(page);
+document.addEventListener('DOMContentLoaded', function () {
+
+    navLinks.forEach(link => {
+        link.addEventListener('click', function () {
+            navLinks.forEach(link => link.classList.remove('active'));
+            this.classList.add('active');
         });
     });
 
-    // This function will load the appropriate html file in the content directory.
-    function loadContent(page) {
-        fetch(page)
-            .then(response => response.text())
-            .then(data => {
-                contentArea.innerHTML = data;
-            })
-            .catch(error => {
-                contentArea.innerHTML = '<h2>Error</h2><p>Sorry, an error occurred while loading the content.</p>';
-                console.error('Error loading content:', error);
-            });
-    }
+    headerNavLink.addEventListener('click', () => {
+        navLinks.forEach(link => link.classList.remove('active'));
+        navLinks[0].classList.add('active');
+    })
+
+    footerNavLink.addEventListener('click', () => {
+        navLinks.forEach(link => link.classList.remove('active'));
+    })
 });
+
+function redirectIframe() {
+    document.getElementById('iframe').src = 'content/home.html';
+}
